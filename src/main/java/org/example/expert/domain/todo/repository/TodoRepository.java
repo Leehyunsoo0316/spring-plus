cd35ed5c6a89e9b66428d41393daf2e142b7f2af
이현수 <mytime0316@naver.com>
2025-03-13 15:29:29 +0900

    Lv2-8 완료

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -10,23 +10,19 @@ import org.springframework.data.repository.query.Param;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
-public interface TodoRepository extends JpaRepository<Todo, Long> {
+public interface TodoRepository extends JpaRepository<Todo, Long>, TodoRepositoryCustom {
 
     @Query("""
             SELECT t FROM Todo t
             WHERE (:weather IS NULL OR t.weather = :weather)
             AND (:startDate IS NULL OR t.modifiedAt >= :startDate)
             AND (:endDate IS NULL OR t.modifiedAt <= :endDate)
-            ORDER BY t.modifiedAt DESC""")
+            ORDER BY t.modifiedAt DESC
+            """)
     Page<Todo> findAllByTodos(
             @Param("weather") String weather,
             @Param("startDate") LocalDateTime startDate,
             @Param("endDate") LocalDateTime endDate,
             Pageable pageable
     );
-
-    @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN t.user " +
-            "WHERE t.id = :todoId")
-    Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
 }
